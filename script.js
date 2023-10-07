352d9df03401c3f5ee3a7fd26a78ee7eb94bbf50
krayyz <mif160500@gmail.com>
2023-10-07 12:40:15 +0700

    assigment_3

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
 // script.js
 
-const baseUrl = 'http://localhost:3000/posts'; // Update the URL to match your endpoint
-
-function createItem() {
-    const createInput = document.getElementById('createInput');
-    const newItem = { title: createInput.value };
-
-    fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newItem),
-    })
-    .then(response => response.json())
-    .then(data => {
-        createInput.value = '';
-        fetchItems();
-    })
-    .catch(error => console.error('Error creating item:', error));
-}
+const baseUrl = 'http://localhost:3000/posts';
 
 function fetchItems() {
     const output = document.getElementById('output');
@@ -38,42 +19,70 @@ function fetchItems() {
     .catch(error => console.error('Error fetching items:', error));
 }
 
+function performCRUDOperation(method, itemData) {
+    fetch(baseUrl, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(itemData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        fetchItems();
+    })
+    .catch(error => console.error(`Error ${method} item:`, error));
+}
+
+function createItem() {
+    const createInput = document.getElementById('createInput');
+    const newItem = { title: createInput.value };
+    performCRUDOperation('POST', newItem);
+    createInput.value = '';
+}
+
 function updateItem() {
     const updateInput = document.getElementById('updateInput');
     const updateValue = document.getElementById('updateValue').value;
     const itemId = updateInput.value;
-
     const updatedItem = { title: updateValue };
-
-    fetch(`${baseUrl}/${itemId}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedItem),
-    })
-    .then(response => response.json())
-    .then(data => {
-        updateInput.value = '';
-        updateValue.value = '';
-        fetchItems();
-    })
-    .catch(error => console.error('Error updating item:', error));
+    performCRUDOperation('PATCH', updatedItem);
+    updateInput.value = '';
+    updateValue.value = '';
 }
 
 function deleteItem() {
     const deleteInput = document.getElementById('deleteInput');
     const itemId = deleteInput.value;
-
-    fetch(`${baseUrl}/${itemId}`, {
-        method: 'DELETE',
-    })
-    .then(response => response.json())
-    .then(() => {
-        deleteInput.value = '';
-        fetchItems();
-    })
-    .catch(error => console.error('Error deleting item:', error));
+    performCRUDOperation('DELETE', null, itemId);
+    deleteInput.value = '';
 }
+
+function getAllItems() {
+    fetchItems();
+}
+
+function getItemById() {
+    const getByIdInput = document.getElementById('getByIdInput');
+    const itemId = getByIdInput.value;
+
+    fetch(`${baseUrl}/${itemId}`)
+    .then(response => response.json())
+    .then(item => {
+        const output = document.getElementById('output');
+        output.innerHTML = '';
+
+        if (item) {
+            const li = document.createElement('li');
+            li.textContent = `${item.id}: ${item.title}`;
+            li.className = 'list-group-item';
+            output.appendChild(li);
+        } else {
+            output.innerHTML = 'Item not found.';
+        }
+    })
+    .catch(error => console.error('Error fetching item by ID:', error));
+}
+
 
 fetchItems();
